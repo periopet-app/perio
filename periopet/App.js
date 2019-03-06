0e25a389e725da18fb3d6462ad8eccce3663e1b4
abkucuk <ahmetbkucuk@gmail.com>
2019-03-06 19:54:26 +0300

    screens ve components dosyaları eklendi, btnAddPhoto.js ve signUp.js dosyaları eklendi

--- periopet/App.js
+++ periopet/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyDZvG2Iss1OMYcvemuVKzLCAeXmLUu7LlI",
+  authDomain: "periopet.firebaseapp.com",
+  databaseURL: "https://periopet.firebaseio.com",
+  projectId: "periopet",
+  storageBucket: "periopet.appspot.com",
+  messagingSenderId: "200177448749"
+};
 
 export default class App extends React.Component {
+  componentWillMount(){
+    firebase.initializeApp(config);
+    
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up Aappppp.js to start working on your app!</Text>
       </View>
     );
   }
