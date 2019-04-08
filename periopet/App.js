d3995fc25ca2bd8cdadcc3af1a3c98fcfec7db71
unknown <ibrahimsabitkaya@gmail.com>
2019-04-08 17:35:27 +0300

    Navigator eklendi

--- periopet/App.js
+++ periopet/App.js
@@ -3,18 +3,38 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, Item, Input, Icon } from 'native-base';
 import SignUp from './screens/signUp';
 import Login from './screens/login';
+import Home from './screens/home'
+
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+
+const SignUpstack= createStackNavigator({
+  SignUp: SignUp,
+})
+
+const HomeStack= createStackNavigator({
+  Home: Home
+})
+
+const AppNavigator = createStackNavigator({
+
+SignUp:SignUpstack,
+Home: HomeStack,
+Login:Login
+
+},
+  { 
+    initialRouteName: 'Login' 
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator,);
 
 
 export default class App extends React.Component {
-  
+
   render() {
-    return (
-      <Container>
-        <SignUp/>
-        
-      </Container>
-      
-    );
+    return <AppContainer></AppContainer>;
   }
 }
 
--- periopet/screens/login.js
+++ periopet/screens/login.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Right,Title,Body,Left,Form, Container, Header, Content, Item, Input, Icon, Thumbnail, Button } from 'native-base';
+import { Right, Title, Body, Left, Form, Container, Header, Content, Item, Input, Icon, Thumbnail, Button } from 'native-base';
+import firebase from 'firebase';
 
+const config = {
+    apiKey: "AIzaSyDZvG2Iss1OMYcvemuVKzLCAeXmLUu7LlI",
+    authDomain: "periopet.firebaseapp.com",
+    databaseURL: "https://periopet.firebaseio.com",
+    projectId: "periopet",
+    storageBucket: "periopet.appspot.com",
+    messagingSenderId: "200177448749"
+};
 export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username:'burak',
-            password:'',
+            username: 'burak',
+            password: '',
         };
     }
 
-onPressButton(u,p){
-    u = this.state.username
+    
 
-}
+    componentWillMount() {
+        firebase.initializeApp(config);
+        console.log('cwm başladı');
+        console.log(firebase.database().ref('users').on('value', data => { data.toJSON() }));
+
+        console.log('cwm bitti');
+
+    };
+
+    onPressButton(u, p) {
+        u = this.state.username
+
+    }
 
     render() {
         return (
             <Container>
-                <Header style={{backgroundColor:'blue'}}>
+                <Header style={{ backgroundColor: 'blue' }}>
                     <Left />
                     <Body>
                         <Title>PERIOPET</Title>
                     </Body>
                     <Right />
                 </Header>
-                <Content style={{ padding: 8 ,marginTop:'6%'}}>
+                <Content style={{ padding: 8, marginTop: '6%' }}>
                     <Form>
                         <Item>
-                            <Input placeholder="E-Mail" onChangeText={ (text) => this.setState({username : text})} />
+                            <Input placeholder="E-Mail" onChangeText={(text) => this.setState({ username: text })} />
                         </Item>
                         <Item last>
                             <Input placeholder="Password" />
@@ -37,10 +57,13 @@ onPressButton(u,p){
                         <Button block style={{ marginTop: 20 }} onPress={this.onPressButton()}>
                             <Text style={{ color: 'white' }}>Giriş</Text>
                         </Button>
-                       <Text style={{marginLeft:'46%',marginTop:20}}>{this.state.username}</Text> 
                         <Button block style={{ marginTop: 20 }}>
-                        <Text style={{ color: 'white' }}>GOOGLE İLE GİRİŞ</Text>
+                            <Text style={{ color: 'white' }}>Google İle Giriş</Text>
                         </Button>
+                        <Button block style={{ marginTop: 20 }} >
+                            <Text style={{ color: 'white' }} onPress = {()=>this.props.navigation.navigate('SignUp')}>Kaydol</Text>
+                        </Button>
+
                     </Form>
                 </Content>
             </Container>
